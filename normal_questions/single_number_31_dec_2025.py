6c78170856bc42a79d7d5dcedc6409529bedca57
Git-Dev-Harshit <harshitsharma8001@gmail.com>
2025-12-31 22:54:58 +0530

    Added optimized verison of program to find single number in an array

--- normal_questions/single_number_31_dec_2025.py
+++ normal_questions/single_number_31_dec_2025.py
@@ -16,7 +16,26 @@ class Solution(object):
         for key, value in counter.items():
             if value == 1:
                 return key
+
+# Optimized version since if we remove .keys() it will check in keys only which is much faster
+# class Solution(object):
+#     def singleNumber(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+#         counter = {}
+
+#         for n in nums:
+#             if n in counter:
+#                 counter[n] += 1
+#             else:
+#                 counter[n] = 1
             
+#         for key, value in counter.items():
+#             if value == 1:
+#                 return key
+
 if __name__ == "__main__":
     a = Solution()
     print(a.singleNumber([2,1,2]))
